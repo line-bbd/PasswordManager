83ffb5b006979c302b55141def430ab2b35961a7
Johan Smit <johansm@bbd.co.za>
2023-04-20 13:16:20 +0200

    Added stuff

--- app/Steps/CreatePasswordStep.cs
+++ app/Steps/CreatePasswordStep.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace PasswordManager.app.Steps
 {
-    internal class ViewPasswordsStep : IStep
+    internal class CreatePasswordStep : IStep
     {
         #region Ctor
 
-        public ViewPasswordsStep() : base()
+        public CreatePasswordStep() : base()
         {
-            _canGoBackTo = true;
+            _canGoBackTo = false;
         }
 
         #endregion
@@ -31,19 +31,19 @@ namespace PasswordManager.app.Steps
 
         public override string GetDisplayOnSelectOption()
         {
-            return SelectOptionsDisplay.VIEW_PASSWORDS_STEP;
+            return SelectOptionsDisplay.CREATE_PASSWORD_STEP;
         }
 
         protected override string GetDisplayOnActivate()
         {
-            return StepTitles.VIEW_PASSWORDS_STEP
+            return StepTitles.CREATE_PASSWORD_STEP
                 + "\n\n"
                 + FetchPasswordsForUser();
         }
 
         protected override string GetBackStep()
         {
-            return "Logout";
+            return "Back";
         }
 
         #endregion
@@ -52,12 +52,12 @@ namespace PasswordManager.app.Steps
 
     public partial class StepTitles
     {
-        public const string VIEW_PASSWORDS_STEP = "Passwords Page";
+        public const string CREATE_PASSWORD_STEP = "Create Password Page";
     }
 
     public partial class SelectOptionsDisplay
     {
-        public const string VIEW_PASSWORDS_STEP = "Continue";
+        public const string CREATE_PASSWORD_STEP = "Create Password";
     }
 
     #endregion
